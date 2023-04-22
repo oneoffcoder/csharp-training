58c5a589ad9a101d52d713be8e626549ed7dc184
Jee Vang, Ph.D <vangjee@gmail.com>
2023-04-22 13:15:49 -0400

    update

---
+++ app6-array/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] names = {
+            "john", "jack", "mary", "mandy"
+        };
+
+        for (int i = 0; i < names.Length; i++)
+        {
+            Console.WriteLine(names[i]);
+        }
+        Console.WriteLine(new string('-', 15));
+
+        foreach (var n in names)
+        {
+            Console.WriteLine(n);
+        }
+        Console.WriteLine(new string('-', 15));
+
+        names.ToList().ForEach(n => Console.WriteLine(n));
+        Console.WriteLine(new string('-', 15));
+    }
+}
