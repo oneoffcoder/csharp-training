cb2f97851f122ec94ca34d2623d54af28b5c2480
Jee Vang, Ph.D <vangjee@gmail.com>
2023-04-25 18:22:48 -0400

    add

---
+++ unity/bouncing-ball-ii/Assets/CircleBehaviourScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleBehaviourScript : MonoBehaviour
+{
+    public GameObject circle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 10; i ++) {
+            float x = Random.Range(-10.0f, 10.0f);
+            float y = Random.Range(-5.0f, 5.0f);
+            float z = 0.0f;
+
+            Debug.Log($"x={x}, y={y}, z={z}");
+            
+            Instantiate(circle, new Vector3(x, y, z), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ unity/bouncing-ball-ii/Assets/GameObjectBehaviourScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        
+    }
+}
