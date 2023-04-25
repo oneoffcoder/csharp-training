d41c374326f09ebb13dc0b442ea371bc0836a03b
Jee Vang, Ph.D <vangjee@gmail.com>
2023-04-25 18:44:46 -0400

    update

--- unity/bouncing-ball-ii/Assets/PrefabCircle.cs
+++ unity/bouncing-ball-ii/Assets/PrefabCircle.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CircleBehaviourScript : MonoBehaviour
+public class PrefabCircle : MonoBehaviour
 {
     public GameObject circle;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +15,7 @@ public class CircleBehaviourScript : MonoBehaviour
 
             Debug.Log($"x={x}, y={y}, z={z}");
             
-            // Instantiate(circle, new Vector3(x, y, z), Quaternion.identity);
+            Instantiate(circle, new Vector3(x, y, z), Quaternion.identity);
         }
     }
 
