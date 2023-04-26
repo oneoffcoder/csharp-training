977001a0be73c1776adf214410f8aefd5dcee1cb
Jee Vang, Ph.D <vangjee@gmail.com>
2023-04-25 23:55:25 -0400

    add

--- unity/bouncing-ball-iii/Assets/CircleBehaviourScript.cs
+++ unity/bouncing-ball-iii/Assets/CircleBehaviourScript.cs
@@ -1,52 +1,80 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CircleBehaviourScript : MonoBehaviour
 {
+    private Camera _camera;
     public float speed = 0.005f;
-    float h = 1.0f;
-    float v = 1.0f;
-    // Start is called before the first frame update
+    private float _h = 1.0f;
+    private float _v = 1.0f;
+    private bool _flip = false;
+    
     void Start()
     {
-        h = Random.Range(0.0f, 1.0f) > 0.5f ? -1.0f : 1.0f;
-        v = Random.Range(0.0f, 1.0f) > 0.5f ? -1.0f : 1.0f;
+        _camera = Camera.main;
+        _h = Random.Range(0.0f, 1.0f) > 0.5f ? -1.0f : 1.0f;
+        _v = Random.Range(0.0f, 1.0f) > 0.5f ? -1.0f : 1.0f;
 
-        float x = Random.Range(-10.0f, 10.0f);
-        float y = Random.Range(-5.0f, 5.0f);
+        var x = Random.Range(-10.0f, 10.0f);
+        var y = Random.Range(-5.0f, 5.0f);
 
         gameObject.transform.position = new Vector2(x, y);
 
-        float r = Random.Range(0.0f, 1.0f);
-        float g = Random.Range(0.0f, 1.0f);
-        float b = Random.Range(0.0f, 1.0f);
-        float alpha = Random.Range(0.5f, 1.0f);
-        Color color = new Color(r, g, b, alpha);
+        var r = Random.Range(0.0f, 1.0f);
+        var g = Random.Range(0.0f, 1.0f);
+        var b = Random.Range(0.0f, 1.0f);
+        var alpha = Random.Range(0.5f, 1.0f);
+        var color = new Color(r, g, b, alpha);
 
         gameObject.GetComponent<Renderer>().material.color = color;
 
-        Debug.Log($"{Screen.width}, {Screen.height}");
+        // Debug.Log($"{Screen.width}, {Screen.height}");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
-        if (pos.x < 0.0 || pos.x > 1.0)
+        if (_camera is null)
         {
-            Debug.Log("left-right edge");
-            h *= -1;
+            return;
         }
-        else if (pos.y < 0.0 || pos.y > 1.0)
+        var pos = _camera.WorldToViewportPoint(transform.position);
+        if (pos.x < 0.0 || pos.x > 1.0 || _flip)
         {
-            Debug.Log("top-bottom edge");
-            v *= -1;
+            // Debug.Log("left-right edge");
+            _h *= -1;
+        }
+        else if (pos.y < 0.0 || pos.y > 1.0 || _flip)
+        {
+            // Debug.Log("top-bottom edge");
+            _v *= -1;
         }
 
-        float x = transform.position.x + (h * speed);
-        float y = transform.position.y + (v * speed);
+        if (_flip)
+        {
+            _flip = false;
+        }
+
+        var position = transform.position;
+        var x = position.x + (_h * speed);
+        var y = position.y + (_v * speed);
 
         gameObject.transform.position = new Vector2(x, y);
+    }
+
+    private void Flip()
+    {
+        _flip = true;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.collider.CompareTag("Circle")) 
+            return;
+
+        var that = collision.collider.GetComponent<CircleBehaviourScript>();
+        that.Flip();
+        this.Flip();
     }
 }
