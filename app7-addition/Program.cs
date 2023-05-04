244b4a9fd70a373d4ce401f39c354666be87ae89
Jee Vang, Ph.D <vangjee@gmail.com>
2023-05-04 17:53:49 -0400

    add

---
+++ app7-addition/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Addition Table Application");
+
+        while (true) {
+            Console.Write("num: ");
+            var query = Console.ReadLine();
+
+            if (query is not null) {
+                if (query.ToLower().Equals("q")) {
+                    break;
+                }
+                
+                var num = Int32.Parse(query);
+
+                for (int i = 0; i <= 12; i++) {
+                    Console.WriteLine($"{i} + {num} = {i + num}");
+                }
+
+                Console.WriteLine(new String('-', 15));
+            }
+        }
+
+        Console.WriteLine("Thanks for using me!");
+    }
+}
