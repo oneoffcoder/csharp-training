3866695d3e4e10facb1f87b98dc70ed7c14e61d9
Jee Vang, Ph.D <vangjee@gmail.com>
2023-07-20 17:29:02 -0400

    add

---
+++ app9-list/Program.cs
@@ -0,0 +1,48 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var names = new List<string> {"john", "jack", "jeff", "jeremy", "jason"};
+        var nameLengths = names.Select(name => name.Length);
+        var firstLetters = names.Select(name => name[0]);
+
+        foreach(var length in nameLengths) {
+            Console.WriteLine(length);
+        }
+
+        Console.WriteLine(new String('-', 15));
+
+        foreach(var letter in firstLetters) {
+            Console.WriteLine(letter);
+        }
+    }
+
+    private static void example3() {
+        var names = new List<string> {"john", "jack", "jeff", "jeremy", "jason"};
+        names.Sort();
+
+        foreach(var name in names) {
+            Console.WriteLine(name);
+        }
+    }
+
+    private static void example2() {
+        var names = new List<string> {"john", "jack", "jeff", "jeremy", "jason"};
+        foreach(var name in names) {
+            Console.WriteLine(name);
+        }
+    }
+
+    private static void example1() {
+        var names = new List<string>();
+        names.Add("john");
+        names.Add("jack");
+        names.Add("jeff");
+        names.Add("jeremy");
+        names.Add("jason");
+        
+        foreach(var name in names) {
+            Console.WriteLine(name);
+        }
+    }
+}
